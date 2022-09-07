6e2f422a9c78cf8f96e5230962323de5b321c35f
Blanco Arturo <blancoarturo92@gmail.com>
2022-09-07 19:45:21 -0300

    se hace correciones de legibilidad

--- 2022-09-05entregable/index.js
+++ 2022-09-05entregable/index.js
@@ -1,23 +1,24 @@
 let readlineSync = require('readline-sync');
+let vueltas = Number (readlineSync.question('Ingrese cantidad de vueltas: '));
 let tiempoVuelta=[];
-let total = 0;
-calcularTiempoVueltas();
-tiempoTotal();
-calcularPromedioVuelta();
+let tiempoTotal = 0;
+cargarTiempoVueltas(vueltas);
+calcularTiempoTotal(vueltas);
+calcularPromedioVuelta(vueltas);
 
-function calcularTiempoVueltas() {
-    for(let i=0 ;i < 4 ; i++) {
+function cargarTiempoVueltas(vueltas) {
+    for(let i=0 ;i < vueltas ; i++) {
     tiempoVuelta[i]= readlineSync.questionInt('Ingrese tiempo de vuelta '+ (i+1) + ' :');
     console.log('El tiempo de la vuelta '+ (i+1) + ' es:' + tiempoVuelta[i]); 
     }
 }
-function tiempoTotal() {
-    for(let i=0 ;i < 4 ; i++) {
-    total += tiempoVuelta[i];
+function calcularTiempoTotal(vueltas) {
+    for(let i=0 ;i < vueltas ; i++) {
+    tiempoTotal += tiempoVuelta[i];
     } 
-console.log('El tiempo total es: '+ total);
+console.log('El tiempo total es: '+ tiempoTotal);
 }
-function calcularPromedioVuelta() {
-    let promedio = total / 4;
+function calcularPromedioVuelta(vueltas) {
+    let promedio = tiempoTotal / vueltas;
     console.log('El promedio de vueltas es: '+promedio);
 }
